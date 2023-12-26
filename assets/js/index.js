6a561a25600f53ef6429b20501750b102e025a24
Noxvill <gmo_mmo@live.cl>
2023-12-26 14:19:41 -0300

    cambios en algoritmos

---
+++ assets/js/index.js
@@ -0,0 +1,97 @@
+// Declaración de variables
+
+let currencyOption = document.getElementById("selectvalue");
+const sectioncurrency = document.getElementById("selectvalue");
+const sectionvaluefinal = document.getElementById("points");
+const apiURL = "https://mindicador.cl/api";
+
+// Fetch para conectar con la api
+
+async function getCurrency() {
+  try {
+    const res = await fetch(apiURL);
+    const currency = await res.json();
+
+    return currency;
+  } catch (e) {
+    alert(e.message);
+  }
+}
+
+// Renderizador de las divisas
+
+async function renderCurrency() {
+  console.log("dentro de rendercurrency");
+  const currencies = await getCurrency();
+  let template = "";
+
+  Object.keys(currencies)
+    .slice(3)
+    .forEach((key) => {
+      const currency = currencies[key];
+      console.log(currency.nombre);
+
+      // alert(currency.nombre)
+      template += `
+        <div class="selectvalue">
+        <option value="${currency.codigo}">${currency.nombre}</option>
+        </div>
+      `;
+    });
+
+  sectioncurrency.innerHTML = template;
+}
+
+renderCurrency();
+
+// Fetch para buscar por id en la api
+var sel;
+async function getCurrencyByid() {
+  sel = currencyOption.value;
+  console.log("Valor seleccionado:", sel);
+  try {
+    const res = await fetch(`https://mindicador.cl/api/${sel}`);
+    const currencyid = await res.json();
+    return currencyid;
+  } catch (e) {
+    alert(e.message);
+  }
+}
+
+// listener para boton
+const btn = document.getElementById("btnconvert");
+btn.addEventListener("click", renderFinalValue);
+var currencyValor = 0;
+
+// Función para calcular el valor final
+
+async function renderFinalValue() {
+  // obtengo el valor de la caja de texto principal
+
+  const inputvalor = document.getElementById("inputclp").value;
+
+  // pregunto si la caja de texto está vacía
+  if (inputvalor == "") {
+    alert("debe ingresar un valor a convertir");
+  } else {
+    // Espero la respuesta con valores desde la consulta a api por id
+    const currencies = await getCurrencyByid();
+    // Obtengo el atributo valor desde la serie
+    const valor = currencies.serie[0].valor;
+
+    // Lo voy a imprimir solo para seguimiento de mi código
+    console.log("El valor de hoy es:" + valor);
+
+    // Lo voy a imprimir solo para seguimiento de mi código
+    console.log("el valor del input es " + inputvalor);
+
+    // Calculo la cnversión
+    let valorfinal = inputvalor * valor;
+
+    console.log("el valor final es: " + valorfinal);
+
+    // imprimo en el dom el resultado
+    document.getElementById("points").innerHTML =
+      "Resultado: " + "$" + valorfinal;
+  }
+}
